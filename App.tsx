b5fd3987d35a346fe0bf5ebaff54af55b4bbaf37
Marcin Izdebski <mizdebski123@gmail.com>
2024-01-26 15:53:07 +0100

    Theme provider added

--- App.tsx
+++ App.tsx
@@ -1,7 +1,11 @@
+import { ThemeProvider } from "styled-components/native";
+import { theme } from "./core/theme";
 
 export default function App() {
   return (
-    null
+    <ThemeProvider theme={theme}>
+
+    </ThemeProvider>
   );
 }
 
