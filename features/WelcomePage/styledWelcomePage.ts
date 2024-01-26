4533082ea0a594b4bae1bcf98875c2bf32ecbfd6
Marcin Izdebski <mizdebski123@gmail.com>
2024-01-26 15:58:02 +0100

    Welcome page elements added

---
+++ features/WelcomePage/styledWelcomePage.ts
@@ -0,0 +1,49 @@
+import { ImageBackground } from "react-native";
+import styled from "styled-components/native";
+
+export const Wrapper = styled.View`
+    flex: 1;
+    justify-content: center;
+    align-items: center;
+`;
+
+export const BackGroundImage = styled.ImageBackground`
+    flex: 1;
+    width: 100%;
+    height: 100%;
+    justify-content: flex-end; 
+    align-items: center; 
+`;
+
+export const Title = styled.Text`
+    color: ${({ theme }) => theme.color.fontMain};
+    font-size: 52px;
+    text-align: center;
+    font-weight: bold;
+`;
+
+export const TitleSpan = styled.Text`
+    color: ${({ theme }) => theme.color.fontSecond};
+
+`;
+
+export const Caption = styled.Text`
+    font-size: 28px;
+    color: ${({ theme }) => theme.color.fontSecond};
+    margin-bottom: 20px;
+`;
+
+export const Button = styled.TouchableOpacity`
+    width: 80%;
+    border-radius: 20px;
+    padding: 12px 10px;
+    background: ${({ theme }) => theme.color.fontMain};
+    margin-bottom: 40px;
+`;
+
+export const ButtonText = styled.Text`
+    text-align: center;
+    font-size: 16px;
+    color: white;
+`;
+
