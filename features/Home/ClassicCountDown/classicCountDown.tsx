f56055fb874e6e187f09c1bc77f988df1e3d85e9
Marcin Izdebski <mizdebski123@gmail.com>
2024-02-01 17:38:29 +0100

    Butotns styles added

--- features/Home/ClassicCountDown/classicCountDown.tsx
+++ features/Home/ClassicCountDown/classicCountDown.tsx
@@ -1,12 +1,23 @@
 import React from 'react';
-import { BackGroundImage, ContentWrapper, Title, Wrapper } from './styledClasicCountDown';
+import { BackGroundImage, ContentWrapper, InputWrapper, NumberOfRounds, Title, Wrapper, Button, ButtonText } from './styledClasicCountDown';
 
 export const ClassicCountDown = () => {
     return (
         <Wrapper>
             <BackGroundImage source={require('../../../common/Images/background.jpg')} >
                 <ContentWrapper>
-                    <Title>Siema</Title>
+                    <Title>Boxing Timer</Title>
+
+                    <InputWrapper>
+                        <Button>
+                            <ButtonText>-</ButtonText>
+                        </Button>
+                        <NumberOfRounds>1</NumberOfRounds>
+                        <Button>
+                            <ButtonText>+</ButtonText>
+                        </Button>
+
+                    </InputWrapper>
                 </ContentWrapper>
             </BackGroundImage>
         </Wrapper>
--- features/Home/ClassicCountDown/styledClasicCountDown.ts
+++ features/Home/ClassicCountDown/styledClasicCountDown.ts
@@ -22,4 +22,34 @@ export const ContentWrapper = styled.View`
 
 export const Title = styled.Text`
     font-size: 40px;
-`; 
+    color: ${({ theme }) => theme.color.fontMain};
+`;
+
+export const InputWrapper = styled.View`
+    flex: 1;
+    flex-wrap: wrap;
+`;
+
+export const Button = styled.TouchableOpacity`
+    border-radius: 800px;
+    padding: 12px;
+    background: ${({ theme }) => theme.color.fontMain};
+    width: 64px;
+    max-height: 64px;
+    flex: 1;
+    justify-content: center;
+    align-items: center;
+`;
+
+export const ButtonText = styled.Text`
+    text-align: center;
+    font-size: 20px;
+    color: white;
+`;
+
+
+
+export const NumberOfRounds = styled.Text`
+    color: ${({ theme }) => theme.color.fontSecond};
+    font-size: 40px;
+`;
